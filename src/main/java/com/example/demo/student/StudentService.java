4a3aa95c5a55a73abdc3541faf6aebbcb83e2f44
damien-d13 <devoti.damien@gmail.com>
2021-10-21 13:47:19 +0200

    Add post

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,26 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRespository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRespository) {
+        this.studentRespository = studentRespository;
+    }
+
+    public List<Student> getStudent() {
+        return studentRespository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
